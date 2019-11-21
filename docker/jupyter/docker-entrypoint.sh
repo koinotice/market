0aa1483fe566f5cd20c4d404e30d7a539808d852
koinotice <koinotice@gmail.com>
2019-11-21 16:18:13 +0800

    adf

--- docker/jupyter/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ $# -eq 0 ]
-  then
-    jupyter lab --ip=0.0.0.0 --NotebookApp.token='local-development' --allow-root --no-browser &> /dev/null
-  else
-    exec "$@"
-fi
